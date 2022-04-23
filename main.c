216a3ee88722a69df42f5bfe622f528821741a51
Capta1n-n9m0 <alievabbas1@gmail.com>
2022-04-23 04:17:18 -0700

    Fps counter and pause button

--- main.c
+++ main.c
@@ -10,17 +10,23 @@
 
 #define MY_WHITE glColor3f(1.0, 1.0, 1.0)
 
+char do_run = 1;
+long long start = 0;
 int square_size = 5;
 int width = 200, height = 200;
 int count = 1;
 board b;
 
-void step();
+long long get_nanoseconds();
+void game_logic();
+void render();
 void draw_board(board board1);
 void draw_cell(int x, int y);
 void draw_square(int x, int y, int size);
 void display();
 void reshape(int w, int h);
+double fps_counter(int resolution_ms);
+void processNormalKeys(unsigned char key, int x, int y);
 
 int main (int argc, char * argv[]) {
     srand(time(NULL));
@@ -28,11 +34,12 @@ int main (int argc, char * argv[]) {
     fill_board_random(b);
     puts("good 29");
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
-
+    //glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+    glutInitDisplayMode(GLUT_RGBA);
     glutInitWindowSize(1000, 1000);
     glutCreateWindow("Game Of Life");
 
+    glutKeyboardFunc(processNormalKeys);
     glutReshapeFunc(reshape);
     glutDisplayFunc(display);
     puts("good 38");
@@ -41,11 +48,44 @@ int main (int argc, char * argv[]) {
     return 0;
 }
 
-void step(){
+void processNormalKeys(unsigned char key, int x, int y){
+    if(key == ' ')
+        do_run = !do_run;
+
+    printf("Key pressed! %c\n", key);
+}
+
+long long get_nanoseconds(){
+    long long res = 0;
+    struct timespec t = {0};
+    clock_gettime(CLOCK_BOOTTIME, &t);
+    res = t.tv_sec * 1000 * 1000 * 1000 + t.tv_nsec;
+    return res;
+}
+
+double fps_counter(int resolution_ms){
+    long long diff = 0;
+    static double fps = 0;
+    if(start == 0)
+        start = get_nanoseconds();
+    if((diff = (get_nanoseconds() - start)) >= resolution_ms * 1000*1000){
+        start = get_nanoseconds();
+        fps = (double) ((long long)count*1000*1000*1000) / diff;
+        count = 0;
+    }
+    return fps;
+}
+
+void game_logic(){
     draw_board(b);
 
-    evolve_board(&b);
-    usleep(30 * 1000);
+    if(do_run)evolve_board(&b);
+    usleep(20 * 1000);
+}
+
+void render(){
+    game_logic();
+    printf("FPS: %.2f\n",fps_counter(100));
 }
 void draw_board(board board1){
     for(int y = 1; y < board1.height; y++)
@@ -70,10 +110,9 @@ void draw_square(int x, int y, int size){
 void display(){
     glClear (GL_COLOR_BUFFER_BIT);
 
-    step();
-    count++;
-    printf("%d\n", count);
+    render();
 
+    count++;
     glutSwapBuffers();
     glutPostRedisplay();
 }
